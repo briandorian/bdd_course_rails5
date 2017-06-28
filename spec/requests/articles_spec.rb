6813955ca0272a68139b32f6eb0ca29bf3487262
briandorian <bacosta70@gmail.com>
2017-06-28 16:13:27 +0000

    handle exception for article not found

---
+++ spec/requests/articles_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.descibe "Articles", type: :request do 
+  before do
+    @article = Article.create(title: "title one", body:"Body of article one")
+  end
+  describe 'GET/articles/:id' do 
+    context 'with existing article' do
+      before { get "/articles/#{@article.id}"}
+      
+      it "handles existing article" do 
+        expect(response.status).to eq 200
+      end
+    end
+    context 'with non-existing article' do 
+      before {get "/articles/xxx" }
+      
+      it "handles non-existing article" do 
+        expect(response.status).to eq 302
+        flash_message= "The article you're looking for couldn't be found"
+        expect(flash[:alert]).to eq flash_message
+      end
+    end
+  end
+end
+
