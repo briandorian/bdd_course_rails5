f6df31a89ee67f6bc21767697a4ffafbcac2ac7f
briandorian <bacosta70@gmail.com>
2017-07-20 15:25:52 +0000

    Delete and Edit buttons shown to the right users or owners

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -2,11 +2,12 @@ require "rails_helper"
 
 RSpec.feature "Showing an article" do 
   before do
-    john = User.create(email: "john@example.com", password: "password")
-    login_as(john)
-    @article = Article.create(title:"The first article",body:"Lorem ipsum dolor sit, consectetus.", user: john) 
+    @john = User.create(email: "john@example.com", password: "password")
+    @fred = User.create(email: "fred@example.com",password: "password")
+    
+    @article = Article.create(title:"The first article",body:"Lorem ipsum dolor sit, consectetus.", user: @john) 
   end
-  scenario "A user shows an articles" do
+  scenario "to non-signed in user hide the Edit and Delete Buttons" do
    
     visit "/"
     
@@ -16,6 +17,36 @@ RSpec.feature "Showing an article" do
     expect(page).to have_content(@article.body)
     
     expect(current_path).to eq(article_path(@article))
-    
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
   end
+ 
+  scenario "to non-owner user hide the Edit and Delete Buttons" do
+    login_as(@fred)
+    visit "/"
+    
+    click_link @article.title
+    
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    
+    expect(current_path).to eq(article_path(@article))
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+  
+  scenario "A signed in owner sees both the Edit and Delete Buttons" do
+    login_as(@john)
+    visit "/"
+        
+    click_link @article.title
+    
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+    
+    expect(page).to have_link("Edit Article")
+    expect(page).to have_link("Delete Article")
+  end
+  
 end
